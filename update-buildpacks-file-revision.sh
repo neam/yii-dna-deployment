e039e60081bdd010d8e858cfbf55c4780e4c6a49
Fredrik Wollsén <fredrik@neam.se>
2014-09-29 17:02:03 +0200

    Switched over to using appsdeck-buildpack-php

--- update-buildpacks-file-revision.sh
+++ update-buildpacks-file-revision.sh
@@ -8,9 +8,9 @@ set -o errexit
 
 script_path=`dirname $0`
 
-export HEROKU_BUILDPACK_PHP_REVISION="$(cd $script_path/heroku-buildpack-php;git rev-parse --verify HEAD)"
+export BUILDPACK_PHP_REVISION="$(cd $script_path/appsdeck-buildpack-php;git rev-parse --verify HEAD)"
 
-sed -i '' 's/heroku-buildpack-php#[^ ]*/heroku-buildpack-php#'$HEROKU_BUILDPACK_PHP_REVISION'/g' '.buildpacks'
+sed -i '' 's/buildpack-php#[^ ]*/buildpack-php#'$BUILDPACK_PHP_REVISION'/g' '.buildpacks'
 
 git  --no-pager diff .buildpacks
 
