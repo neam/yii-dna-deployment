6be507ff8e3baef2547ee6407db951d0b87c1ca2
Fredrik Wollsén <fredrik@neam.se>
2015-04-09 19:42:20 +0300

    Script to upload current media as public files

---
+++ util/upload-current-media-as-public-files.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# fail on any error
+set -o errexit
+
+# Uncomment to see all variables used in this sciprt
+set -x;
+
+script_path=$(dirname $0)
+dna_path=$script_path/../../../../dna
+media_path=$dna_path/db/data/p3media
+
+# make app config available as shell variables
+php vendor/neam/php-app-config/export.php | tee /tmp/php-app-config.sh
+source /tmp/php-app-config.sh
+
+# configure s3cmd
+echo "[default]
+access_key = $PUBLIC_FILE_UPLOADERS_ACCESS_KEY
+secret_key = $PUBLIC_FILE_UPLOADERS_SECRET" > /tmp/.public-files.s3cfg
+
+if [ "$PUBLIC_FILES_S3_PATH" == "" ]; then
+  echo "Empty PUBLIC_FILES_S3_PATH is dangerous"
+  exit 1
+fi
+
+# upload current media to public files
+s3cmd -v --acl-public --config=/tmp/.public-files.s3cfg --recursive sync $media_path/ "${PUBLIC_FILES_S3_BUCKET}${PUBLIC_FILES_S3_PATH}media/"
+
+exit 0
