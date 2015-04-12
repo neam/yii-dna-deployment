cc51049bf9286416bbf36e66dbfdb89b96cf19bc
Fredrik Wollsén <fredrik@neam.se>
2015-04-12 16:24:45 +0300

    Building php and nginx images with source code

--- deploy/build.sh
+++ deploy/build.sh
@@ -9,10 +9,13 @@ set -o errexit
 # build and push src to tutum
 
 set -x
-docker build -f .src.builder.Dockerfile -t $DOCKER_REGISTRY_USER/$REPO:git-commit-$COMMITSHA .
-docker tag -f $DOCKER_REGISTRY_USER/$REPO:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO:git-commit-$COMMITSHA
+docker build -f .stack.php.Dockerfile -t $DOCKER_REGISTRY_USER/$REPO-php:git-commit-$COMMITSHA .
+docker build -f .stack.nginx.Dockerfile -t $DOCKER_REGISTRY_USER/$REPO-nginx:git-commit-$COMMITSHA .
+docker tag -f $DOCKER_REGISTRY_USER/$REPO-php:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO-nginx:git-commit-$COMMITSHA
+docker tag -f $DOCKER_REGISTRY_USER/$REPO-nginx:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO-php:git-commit-$COMMITSHA
 docker login --email=$TUTUM_EMAIL --username=$TUTUM_USER --password=$TUTUM_PASSWORD https://tutum.co/v1
-docker push tutum.co/$TUTUM_USER/$REPO:git-commit-$COMMITSHA
+docker push tutum.co/$TUTUM_USER/$REPO-php:git-commit-$COMMITSHA
+docker push tutum.co/$TUTUM_USER/$REPO-nginx:git-commit-$COMMITSHA
 set +x
 
 # display deployment instructions
