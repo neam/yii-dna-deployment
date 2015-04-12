eb2ac558d86fef4106dc6607c157920d8c155a90
Fredrik Wollsén <fredrik@neam.se>
2015-04-12 17:54:03 +0300

    Fixed typo

--- deploy/build.sh
+++ deploy/build.sh
@@ -11,8 +11,8 @@ set -o errexit
 set -x
 docker build -f .stack.php.Dockerfile -t $DOCKER_REGISTRY_USER/$REPO-php:git-commit-$COMMITSHA .
 docker build -f .stack.nginx.Dockerfile -t $DOCKER_REGISTRY_USER/$REPO-nginx:git-commit-$COMMITSHA .
-docker tag -f $DOCKER_REGISTRY_USER/$REPO-php:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO-nginx:git-commit-$COMMITSHA
-docker tag -f $DOCKER_REGISTRY_USER/$REPO-nginx:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO-php:git-commit-$COMMITSHA
+docker tag -f $DOCKER_REGISTRY_USER/$REPO-php:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO-php:git-commit-$COMMITSHA
+docker tag -f $DOCKER_REGISTRY_USER/$REPO-nginx:git-commit-$COMMITSHA tutum.co/$TUTUM_USER/$REPO-nginx:git-commit-$COMMITSHA
 docker login --email=$TUTUM_EMAIL --username=$TUTUM_USER --password=$TUTUM_PASSWORD https://tutum.co/v1
 docker push tutum.co/$TUTUM_USER/$REPO-php:git-commit-$COMMITSHA
 docker push tutum.co/$TUTUM_USER/$REPO-nginx:git-commit-$COMMITSHA
