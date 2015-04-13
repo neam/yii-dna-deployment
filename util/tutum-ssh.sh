9b9f9f27b5561618f74edd82c438073d31299fc3
Fredrik Wollsén <fredrik@neam.se>
2015-04-13 12:56:55 +0300

    Enriched tutum ssh script output

--- util/tutum-ssh.sh
+++ util/tutum-ssh.sh
@@ -9,6 +9,19 @@ tutum container inspect $WORKER_CONTAINER_ID > .tutum-worker-container.json
 SSH_PORT=$(cat .tutum-worker-container.json | jq '.container_ports[0].outer_port')
 SSH_FQDN=$(cat .tutum-worker-container.json | jq -r '.link_variables.WORKER_ENV_TUTUM_NODE_FQDN')
 
-echo "ssh -p $SSH_PORT root@$SSH_FQDN"
+echo "Init and connect:"
+echo "export SSH_PORT=$SSH_PORT"
+echo "export SSH_FQDN=$SSH_FQDN"
+echo 'scp -r -P $SSH_PORT $DEPLOYMENTS_ROOT/$APPVHOST/.env root@$SSH_FQDN:/.env'
+echo 'scp -r -P $SSH_PORT ~/.ssh/id_rsa root@$SSH_FQDN:/root/.ssh/id_rsa'
+echo 'ssh -p $SSH_PORT root@$SSH_FQDN'
+echo
+echo "When connected:"
+echo "git clone --recursive REPO /app"
+echo "git checkout SHA"
+echo "PREFER=dist stack/src/install-deps.sh"
+echo "source /.env"
+echo
+echo "Then run commands"
 
 exit 0
