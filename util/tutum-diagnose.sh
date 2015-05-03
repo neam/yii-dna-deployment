411f1dd5489db9169c55cf0e458bf33aafc929be
Fredrik Wollsén <fredrik@neam.se>
2015-05-03 12:34:37 +0300

    Various improvements to tutum diagnose output

--- util/tutum-diagnose.sh
+++ util/tutum-diagnose.sh
@@ -41,26 +41,34 @@ echo "  deploy/generate-config.sh"
 STACK_ID=$(cat .tutum-stack-id)
 tutum stack inspect $STACK_ID > .tutum-stack.json
 
+if [ "$(cat .tutum-stack.json | jq -r '.state')" == "Terminated" ]; then
+    echo "Stack is terminated";
+    exit 0;
+fi
+
 echo
 echo "# The stack's non-public containers are running on the following nodes:"
 cat .tutum-stack.json | jq '.services' | grep _ENV_TUTUM_NODE_FQDN | grep -v '"key"' | grep '_\d_' | sed 's/^/{/' | sed 's/,/}/' | jq -s add | tee .tutum-containers-nodes
 echo
 echo "# To ssh into the tutum nodes and run further diagnostics:"
 cat .tutum-containers-nodes | jq -r '.[]' | sort -u | sed 's/^/ssh root@/'
+echo
+echo "# Or, using mosh"
+cat .tutum-containers-nodes | jq -r '.[]' | sort -u | sed 's/^/mosh root@/'
 
 WORKER_CONTAINER_ID=$(cat .tutum-stack.json | jq '.services | map(select(.name == "worker"))' | jq -r '.[0].containers[0]' | awk -F  "/" '{print $5}')
 tutum container inspect $WORKER_CONTAINER_ID > .tutum-worker-container.json
 WEB_CONTAINER_ID=$(cat .tutum-stack.json | jq '.services | map(select(.run_command == "/app/stack/nginx/run.sh"))' | jq -r '.[0].containers[0]' | awk -F  "/" '{print $5}')
 tutum container inspect $WEB_CONTAINER_ID > .tutum-web-container.json
+PHPHAPROXY_CONTAINER_ID=$(cat .tutum-stack.json | jq '.services | map(select(.name == "phphaproxy"))' | jq -r '.[0].containers[0]' | awk -F  "/" '{print $5}')
+tutum container inspect $PHPHAPROXY_CONTAINER_ID > .tutum-phphaproxy-container.json
 
-
-WEB_CONTAINER_ID=$(cat .tutum-stack.json | jq '.services | map(select(.run_command == "/app/stack/nginx/run.sh"))' | jq -r '.[0].containers[0]' | awk -F  "/" '{print $5}')
-tutum container inspect $WEB_CONTAINER_ID > .tutum-web-container.json
-
-SSH_PORT=$(cat .tutum-worker-container.json | jq '.container_ports[0].outer_port')
+SSH_PORT=$(cat .tutum-worker-container.json | jq '.container_ports  | map(select(.inner_port == 22)) | .[].outer_port')
 SSH_FQDN=$(cat .tutum-worker-container.json | jq -r '.link_variables.WORKER_ENV_TUTUM_NODE_FQDN')
-WEB_PORT=$(cat .tutum-web-container.json | jq '.container_ports[1].outer_port')
+WEB_PORT=$(cat .tutum-web-container.json | jq '.container_ports  | map(select(.inner_port == 80)) | .[].outer_port')
 WEB_FQDN=$(cat .tutum-web-container.json | jq '.link_variables' | grep '"WEB.*_ENV_TUTUM_NODE_FQDN' | sed 's/^/{/' | sed 's/,/}/' | grep -v '_\d_' | jq -r '.[]')
+PHPHAPROXY_STATS_PORT=$(cat .tutum-phphaproxy-container.json | jq '.container_ports  | map(select(.inner_port == 1936)) | .[].outer_port')
+PHPHAPROXY_STATS_FQDN=$(cat .tutum-phphaproxy-container.json | jq '.link_variables' | grep '"WEB.*_ENV_TUTUM_NODE_FQDN' | sed 's/^/{/' | sed 's/,/}/' | grep -v '_\d_' | jq -r '.[]')
 
 echo
 echo "# Health-checks for public frontend:"
@@ -72,5 +80,10 @@ echo "# Health-checks for first-level backend (Nginx):"
 echo "export WEB_PORT=$WEB_PORT"
 echo "export WEB_FQDN=$WEB_FQDN"
 echo "stack/_util/health-checks.sh"
+echo
+echo "# Stats for second-level backend (PHP haproxy):"
+echo "export PHPHAPROXY_STATS_PORT=$WEB_PORT"
+echo "export PHPHAPROXY_STATS_FQDN=$WEB_FQDN"
+echo 'open http://$PHPHAPROXY_STATS_FQDN:$PHPHAPROXY_STATS_PORT'
 echo
 exit 0
