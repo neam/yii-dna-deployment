3b326957745a5beb332b90f6e36c5f3074232020
Fredrik Wollsén <fredrik@neam.se>
2015-09-07 13:15:49 +0300

    Updated secrets.dist file

--- skeleton/deploy/config/secrets.dist.php
+++ skeleton/deploy/config/secrets.dist.php
@@ -20,7 +20,10 @@ $_ENV["USER_DATA_BACKUP_UPLOADERS_ACCESS_KEY"] = "";
 $_ENV["USER_DATA_BACKUP_UPLOADERS_SECRET"] = "";
 $_ENV["PUBLIC_FILE_UPLOADERS_ACCESS_KEY"] = "";
 $_ENV["PUBLIC_FILE_UPLOADERS_SECRET"] = "";
-$_ENV["PUBLIC_FILES_S3_REGION"] = "eu-west-1";
+$_ENV["PUBLIC_FILES_S3_REGION"] = "us-standard";
+$_ENV["PUBLIC_FILES_S3_PATH"] = "/m/" . Config::read("APPVHOST") . "/";
+$_ENV["CDN_PATH_HTTP"] = "http://static.example.com" . $_ENV["PUBLIC_FILES_S3_PATH"];
+$_ENV["CDN_PATH_HTTPS"] = "https://static.example.com" . $_ENV["PUBLIC_FILES_S3_PATH"];
 
 $_ENV["COMPOSER_GITHUB_TOKEN"] = "";
 $_ENV["TUTUM_USER"] = "";
@@ -29,10 +32,23 @@ $_ENV["TUTUM_PASSWORD"] = "";
 $_ENV["TUTUM_APIKEY"] = "";
 $_ENV["NEW_RELIC_LICENSE_KEY"] = "";
 
-$_ENV["DEVELOPMENT_SMTP_URL"] = "smtp://mailcatcher:25";
-$_ENV["PRODUCTION_SMTP_URL"] = "";
+$_ENV["DEVELOPMENT_SMTP_HOST"] = "mailcatcher";
+$_ENV["DEVELOPMENT_SMTP_PORT"] = "25";
+$_ENV["DEVELOPMENT_SMTP_USERNAME"] = "foo";
+$_ENV["DEVELOPMENT_SMTP_PASSWORD"] = "bar";
+$_ENV["DEVELOPMENT_SMTP_ENCRYPTION"] = "foo";
+
+$_ENV["PRODUCTION_SMTP_HOST"] = "smtp.example.com";
+$_ENV["PRODUCTION_SMTP_PORT"] = "587";
+$_ENV["PRODUCTION_SMTP_USERNAME"] = "changeme";
+$_ENV["PRODUCTION_SMTP_PASSWORD"] = "changeme";
+$_ENV["PRODUCTION_SMTP_ENCRYPTION"] = "tls";
 
 $_ENV["FILEPICKER_API_KEY"] = "";
+
+$_ENV["AUTH0_CLIENT_ID"] = "";
+$_ENV["AUTH0_CLIENT_SECRET"] = "";
+$_ENV["CORS_ACL_ORIGIN_HOSTS"] = "localhost:9000,app.example.com";
 
 $_ENV["DEVELOPMENT_GA_TRACKING_ID"] = "";
 $_ENV["PRODUCTION_GA_TRACKING_ID"] = "";
@@ -43,25 +59,20 @@ $_ENV["SENTRY_DSN"] = "";
 $_ENV['WEB_SERVER_POSIX_USER'] = "www-data";
 $_ENV['WEB_SERVER_POSIX_GROUP'] = "www-data";
 
-// SMS Messaging service Twilio
-$_ENV["TWILIO_ACCOUNT_SID"] = "AC8c7a2821177ed021d4b2441f9b837a90";
-$_ENV["TWILIO_FROM_NUMBER"] = "+46769446600";
-$_ENV["TWILIO_AUTH_TOKEN"] = "";
-
-// Heywatch
-$_ENV["HEYWATCH_API_USERNAME"] = "";
-$_ENV["HEYWATCH_API_KEY"] = "";
-$_ENV["ENCODING_FILES_S3_API_KEY"] = "";
-$_ENV["ENCODING_FILES_S3_SECRET"] = "";
-$_ENV["ENCODING_FILES_S3_BUCKET"] = "s3://encoding.adoveo.com";
-$_ENV["ENCODING_FILES_S3_REGION"] = "eu-west-1";
-
 // Smtp url
-if (Config::read("BRANCH_TO_DEPLOY") === "master") {
-    $_ENV["SMTP_URL"] = $_ENV["PRODUCTION_SMTP_URL"];
+if (Config::read("DEPLOY_STABILITY_TAG") === "prod") {
+    $_ENV["SMTP_HOST"] = $_ENV["PRODUCTION_SMTP_HOST"];
+    $_ENV["SMTP_PORT"] = $_ENV["PRODUCTION_SMTP_PORT"];
+    $_ENV["SMTP_USERNAME"] = $_ENV["PRODUCTION_SMTP_USERNAME"];
+    $_ENV["SMTP_PASSWORD"] = $_ENV["PRODUCTION_SMTP_PASSWORD"];
+    $_ENV["SMTP_ENCRYPTION"] = $_ENV["PRODUCTION_SMTP_ENCRYPTION"];
     $_ENV["GA_TRACKING_ID"] = $_ENV["DEVELOPMENT_GA_TRACKING_ID"];
 } else {
-    $_ENV["SMTP_URL"] = $_ENV["DEVELOPMENT_SMTP_URL"];
+    $_ENV["SMTP_HOST"] = $_ENV["DEVELOPMENT_SMTP_HOST"];
+    $_ENV["SMTP_PORT"] = $_ENV["DEVELOPMENT_SMTP_PORT"];
+    $_ENV["SMTP_USERNAME"] = $_ENV["DEVELOPMENT_SMTP_USERNAME"];
+    $_ENV["SMTP_PASSWORD"] = $_ENV["DEVELOPMENT_SMTP_PASSWORD"];
+    $_ENV["SMTP_ENCRYPTION"] = $_ENV["DEVELOPMENT_SMTP_ENCRYPTION"];
     $_ENV["GA_TRACKING_ID"] = $_ENV["DEVELOPMENT_GA_TRACKING_ID"];
 }
 
@@ -70,13 +81,13 @@ if (Config::read("BRANCH_TO_DEPLOY") === "master") {
 $_ENV["DEV_RDS_HOST"] = "";
 $_ENV["PROD_RDS_HOST"] = "";
 $_ENV["DATABASE_ROOT_USER"] = "";
-$_ENV["DATABASE_ROOT_PASSWORD"] = ""; // Tmp using hardcoded details for rds dev-1
+$_ENV["DATABASE_ROOT_PASSWORD"] = "";
 
 // Amazon RDS app access details
 
 $app = Config::read("APPVHOST");
 switch ($app) {
-    case "develop-foo.adoveodev.com":
+    case "develop-foo.example.com":
         $_ENV["DATABASE_HOST"] = "";
         $_ENV["DATABASE_PORT"] = "";
         $_ENV["DATABASE_USER"] = "";
