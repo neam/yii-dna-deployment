ad96e07529d01b209690efe534e9c2b0f3c6a222
Fredrik Wollsén <fredrik@neam.se>
2015-10-14 11:00:24 +0300

    Minor tweaks

--- deploy/generate-config.php
+++ deploy/generate-config.php
@@ -10,7 +10,7 @@ require(dirname(
 
 // Add deployment-only overrides
 
-$deploymentEnvPath = "deployments/" . Config::read("APPVHOST");
+$deploymentEnvPath = "deployments/" . Config::read("APPVHOST", null, $required = true);
 $_ENV['CONFIG_INCLUDE'] = "config/remote/include.php";
 Config::expect("CONFIG_INCLUDE", $default = null, $required = true);
 Config::expect("ENV", $default = $deploymentEnvPath, $required = false);
@@ -20,10 +20,6 @@ Config::expect("ENV", $default = $deploymentEnvPath, $required = false);
 Config::expect("BRANCH_TO_DEPLOY", $default = null, $required = true);
 Config::expect("PROJECT_GIT_REPO", $default = null, $required = true);
 Config::expect("COMMITSHA", $default = null, $required = true);
-
-// Loads sensitive (non-versioned) environment variables from .env to getenv(), $_ENV.
-//\Dotenv::makeMutable();
-//\Dotenv::load($project_root . '/' . $deploymentEnvPath);
 
 // Include the secrets file containing non-versioned secrets
 require(getenv('BUILD_DIR') . DIRECTORY_SEPARATOR . 'deploy/config/secrets.php');
