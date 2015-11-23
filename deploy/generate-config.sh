42bed1e756f2d69a4b593a778ac5648c724ead2c
Fredrik Wollsén <fredrik@neam.se>
2015-11-23 13:50:00 +0200

    Better error handling and clearer instructions output

--- deploy/generate-config.sh
+++ deploy/generate-config.sh
@@ -53,53 +53,57 @@ mkdir -p "$DEPLOYMENT_DIR"
 
 export CONFIG_INCLUDE=vendor/neam/yii-dna-deployment/deploy/generate-config.php
 
-echo
-echo 'Config for '$APPVHOST':'
-echo
+# dry-run config export to catch errors during the process
+set +e
+php vendor/neam/php-app-config/export.php > $DEPLOYMENT_DIR/.env
+if [ ! "$?" == "0" ]; then
+  cat $DEPLOYMENT_DIR/.env
+  exit 1;
+fi
+set -e
 
 # we ignore runtime-config which are set on the fly
 php vendor/neam/php-app-config/export.php | grep -v 'export DATABASE_USER=' | grep -v 'export DATA=' | grep -v 'export DATABASE_NAME=' > $DEPLOYMENT_DIR/.env
 
-if [ "$?" == "0" ]; then
-    source $DEPLOYMENT_DIR/.env
-
-    # prepare stack yml
-
-    cat stack/docker-compose-production.yml \
-     | sed 's|%COMMITSHA%|'$COMMITSHA'|' \
-     | sed 's|%ENV_FILE_DIR%|.|' \
-     | sed 's|%APPVHOST%|'$APPVHOST'|' \
-     | sed 's|%DEPLOY_STABILITY_TAG%|'$DEPLOY_STABILITY_TAG'|' \
-     | sed 's|%TUTUM_USER%|'$TUTUM_USER'|' \
-     | sed 's|%REPO%|'$REPO'|' \
-     | sed 's|%VIRTUAL_HOST%|'"$(sedescape "$VIRTUAL_HOST")"'|' \
-     > $DEPLOYMENT_DIR/docker-compose-production.yml
-
-    cat $DEPLOYMENT_DIR/.env \
-     | grep -v '='"''" \
-     | sed 's|export |    |' \
-     | sed 's|='"'"'|: '"'"'|' \
-     | sed 's|\\\?|\?|' \
-     > $DEPLOYMENT_DIR/.env.yml
-
-    VIRTUAL_HOST_BASED_WEB_SERVICE_NAME=$(servicename "web${APPVHOST}${COMMITSHA}")
-
-    cat stack/docker-compose-production-tutum.yml \
-     | sed 's|%COMMITSHA%|'$COMMITSHA'|' \
-     | sed 's|%APPVHOST%|'$APPVHOST'|' \
-     | sed 's|%DEPLOY_STABILITY_TAG%|'$DEPLOY_STABILITY_TAG'|' \
-     | sed 's|%TUTUM_USER%|'$TUTUM_USER'|' \
-     | sed 's|%REPO%|'$REPO'|' \
-     | sed 's|%VIRTUAL_HOST%|'"$(sedescape "$VIRTUAL_HOST")"'|' \
-     | sed 's|%VIRTUAL_HOST_BASED_WEB_SERVICE_NAME%|'$VIRTUAL_HOST_BASED_WEB_SERVICE_NAME'|' \
-     > $DEPLOYMENT_DIR/docker-compose-production-tutum.yml
-
-    sed -e '/ENVIRONMENT_YAML/ {' -e 'r '"$DEPLOYMENT_DIR/.env.yml" -e 'd' -e '}' -i '' $DEPLOYMENT_DIR/docker-compose-production-tutum.yml
-
-fi
-
-cat $DEPLOYMENT_DIR/.env
 echo
+echo 'Config for '$APPVHOST':'
+echo
+cat $DEPLOYMENT_DIR/.env
+
+source $DEPLOYMENT_DIR/.env
+
+# prepare stack yml
+
+cat stack/docker-compose-production.yml \
+ | sed 's|%COMMITSHA%|'$COMMITSHA'|' \
+ | sed 's|%ENV_FILE_DIR%|.|' \
+ | sed 's|%APPVHOST%|'$APPVHOST'|' \
+ | sed 's|%DEPLOY_STABILITY_TAG%|'$DEPLOY_STABILITY_TAG'|' \
+ | sed 's|%TUTUM_USER%|'$TUTUM_USER'|' \
+ | sed 's|%REPO%|'$REPO'|' \
+ | sed 's|%VIRTUAL_HOST%|'"$(sedescape "$VIRTUAL_HOST")"'|' \
+ > $DEPLOYMENT_DIR/docker-compose-production.yml
+
+cat $DEPLOYMENT_DIR/.env \
+ | grep -v '='"''" \
+ | sed 's|export |    |' \
+ | sed 's|='"'"'|: '"'"'|' \
+ | sed 's|\\\?|\?|' \
+ > $DEPLOYMENT_DIR/.env.yml
+
+VIRTUAL_HOST_BASED_WEB_SERVICE_NAME=$(servicename "web${APPVHOST}${COMMITSHA}")
+
+cat stack/docker-compose-production-tutum.yml \
+ | sed 's|%COMMITSHA%|'$COMMITSHA'|' \
+ | sed 's|%APPVHOST%|'$APPVHOST'|' \
+ | sed 's|%DEPLOY_STABILITY_TAG%|'$DEPLOY_STABILITY_TAG'|' \
+ | sed 's|%TUTUM_USER%|'$TUTUM_USER'|' \
+ | sed 's|%REPO%|'$REPO'|' \
+ | sed 's|%VIRTUAL_HOST%|'"$(sedescape "$VIRTUAL_HOST")"'|' \
+ | sed 's|%VIRTUAL_HOST_BASED_WEB_SERVICE_NAME%|'$VIRTUAL_HOST_BASED_WEB_SERVICE_NAME'|' \
+ > $DEPLOYMENT_DIR/docker-compose-production-tutum.yml
+
+sed -e '/ENVIRONMENT_YAML/ {' -e 'r '"$DEPLOYMENT_DIR/.env.yml" -e 'd' -e '}' -i '' $DEPLOYMENT_DIR/docker-compose-production-tutum.yml
 
 # prepare new db
 
@@ -108,7 +112,7 @@ if [ "$DATABASE_HOST" == "" ]; then
 fi
 
 echo
-echo 'If no errors are shown above, config is prepared for '$APPVHOST'. To build images and push to tutum registry:'
+echo 'Config is prepared for '$APPVHOST'. To build images and push to tutum registry:'
 echo
 echo "  vendor/neam/yii-dna-deployment/deploy/build.sh"
 echo
@@ -117,13 +121,15 @@ echo
 echo "  export TUTUM_USER=\$TUTUM_USER"
 echo "  export TUTUM_APIKEY=\$TUTUM_APIKEY"
 echo
-echo 'Then, run one of the following to deploy:'
+echo 'Then, to deploy to tutum:'
 echo
 echo "  tutum stack create --name=$STACK_NAME -f $DEPLOYMENT_DIR/docker-compose-production-tutum.yml | tee $DEPLOYMENT_DIR/.tutum-stack-id && \\"
 echo "  tutum stack start \$(cat $DEPLOYMENT_DIR/.tutum-stack-id)"
+#echo
+#echo "  tutum stack update -f $DEPLOYMENT_DIR/docker-compose-production-tutum.yml \$(cat $DEPLOYMENT_DIR/.tutum-stack-id)"
+#echo "  tutum stack redeploy \$(cat $DEPLOYMENT_DIR/.tutum-stack-id)"
 echo
-echo "  tutum stack update -f $DEPLOYMENT_DIR/docker-compose-production-tutum.yml \$(cat $DEPLOYMENT_DIR/.tutum-stack-id)"
-echo "  tutum stack redeploy \$(cat $DEPLOYMENT_DIR/.tutum-stack-id)"
+echo 'To deploy to the current docker host:'
 echo
 echo "  docker-compose --project-name $APPVHOST -f $DEPLOYMENT_DIR/docker-compose-production.yml up -d"
 echo
