8f50010852c753fda3fa8a936ddce61bf86a06a7
Fredrik Wollsén <fredrik@neam.se>
2016-05-19 15:34:11 +0300

    De-duplication code refactoring

--- deploy/build.sh
+++ deploy/build.sh
@@ -6,13 +6,22 @@ set -o errexit
 # debug
 #set -x
 
+function build_and_push_src_image_for_service_in_stack {
+
+    local SERVICE=$1
+
+    docker build -f .stack.$SERVICE.Dockerfile -t $DOCKERCLOUD_USER/$REPO-$SERVICE:git-commit-$COMMITSHA .
+    docker push $DOCKERCLOUD_USER/$REPO-$SERVICE:git-commit-$COMMITSHA
+
+}
+
 # build and push src to docker-cloud
 
 set -x
-docker build -f .stack.php.Dockerfile -t $DOCKERCLOUD_USER/$REPO-php:git-commit-$COMMITSHA .
-docker build -f .stack.nginx.Dockerfile -t $DOCKERCLOUD_USER/$REPO-nginx:git-commit-$COMMITSHA .
-docker push $DOCKERCLOUD_USER/$REPO-php:git-commit-$COMMITSHA
-docker push $DOCKERCLOUD_USER/$REPO-nginx:git-commit-$COMMITSHA
+
+build_and_push_src_image_for_service_in_stack php
+build_and_push_src_image_for_service_in_stack nginx
+
 set +x
 
 # display deployment instructions
